d91b6ed257f9a0a7b83efdf749d9ba7831ca0b0a
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-08-31 14:44:34 +0200

    Added logic to how computer plays the game

--- TicTacToe/ViewModel/TicTacToeViewModel.cs
+++ TicTacToe/ViewModel/TicTacToeViewModel.cs
@@ -35,50 +35,112 @@ namespace TicTacToe.ViewModel
             }
         }
         private void FieldClickExecute(object param)
-        {
+        {            
             Button btn = (Button)param;
             int row = Grid.GetRow(btn);
             int column = Grid.GetColumn(btn);
             ticTacToeGrid[row - 1, column - 1] = "X";
             btn.Content = "X";
-            view.lblStatus.Content = "Computer is thinking.";
-            Thread.Sleep(500);
-            view.lblStatus.Content = "Computer is thinking..";
-            Thread.Sleep(500);
-            view.lblStatus.Content = "Computer is thinking...";
-            Thread.Sleep(500);
+
+            if (CheckVictory())
+            {
+                if (MessageBox.Show("Player won!/nWould you like to play again?","Congratulations",MessageBoxButton.YesNo,MessageBoxImage.None) == MessageBoxResult.Yes)
+                {
+                    TicTacToeView newGame = new TicTacToeView();
+                    newGame.Show();
+                    view.Close();
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }     
+            row = rnd.Next(0,3);
+            column = rnd.Next(0, 3);
+            int rowO;
+            int columnO;
             do
             {
+                rowO = rnd.Next(0, 2);
+                columnO = rnd.Next(0, 2);
+            } while (CheckFreeSpot(row,column));
+            Button btn1 = (Button)GetGridElement(view.gameGrid, rowO+1, columnO+1);
+            ticTacToeGrid[rowO, columnO] = "O";
+            btn1.Content = "O";
 
-            } while ();
-            
+            if (CheckVictory())
+            {
+                if (MessageBox.Show("Computer won!/nWould you like to play again?", "Game Over", MessageBoxButton.YesNo, MessageBoxImage.None) == MessageBoxResult.Yes)
+                {
+                    TicTacToeView newGame = new TicTacToeView();
+                    newGame.Show();
+                    view.Close();
+                }
+                else
+                {
+                    view.Close();
+                }
+            }
+
         }
         private bool CanFieldClickExecute(object param)
         {
             return true;
+            //Button btn = (Button)param;
+            //if (btn.Content == null)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            //int row = Grid.GetRow(btn);
+            //int column = Grid.GetColumn(btn);
+            //return CheckFreeSpot(row, column);
         }
 
-        public bool Victory()
+        public bool CheckVictory()
         {
-            if ((ticTacToeGrid[0, 0] == ticTacToeGrid[0, 1] && ticTacToeGrid[0, 1] == ticTacToeGrid[0, 2])
-                || (ticTacToeGrid[1, 0] == ticTacToeGrid[1, 1] && ticTacToeGrid[1, 1] == ticTacToeGrid[1, 2])
-                || (ticTacToeGrid[2, 0] == ticTacToeGrid[2, 1] && ticTacToeGrid[2, 1] == ticTacToeGrid[2, 2])
-                || (ticTacToeGrid[0, 0] == ticTacToeGrid[1, 0] && ticTacToeGrid[1, 0] == ticTacToeGrid[2, 0])
-                || (ticTacToeGrid[0, 1] == ticTacToeGrid[1, 1] && ticTacToeGrid[1, 1] == ticTacToeGrid[2, 1])
-                || (ticTacToeGrid[0, 2] == ticTacToeGrid[1, 2] && ticTacToeGrid[1, 2] == ticTacToeGrid[2, 2])
-                || (ticTacToeGrid[0, 0] == ticTacToeGrid[1, 1] && ticTacToeGrid[1, 1] == ticTacToeGrid[2, 2])
-                || (ticTacToeGrid[0, 2] == ticTacToeGrid[1, 1] && ticTacToeGrid[1, 1] == ticTacToeGrid[2, 0]))
+            if ((ticTacToeGrid[0, 0] == "X" && ticTacToeGrid[0, 1] == "X" && ticTacToeGrid[0, 2]=="X")
+                || (ticTacToeGrid[1, 0] == "X" && ticTacToeGrid[1, 1] == "X" && ticTacToeGrid[1, 2] == "X")
+                || (ticTacToeGrid[2, 0] == "X" && ticTacToeGrid[2, 1] == "X" && ticTacToeGrid[2, 2] == "X")
+                || (ticTacToeGrid[0, 0] == "O" && ticTacToeGrid[0, 1] == "O" && ticTacToeGrid[0, 2] == "O")
+                || (ticTacToeGrid[1, 0] == "O" && ticTacToeGrid[1, 1] == "O" && ticTacToeGrid[1, 2] == "O")
+                || (ticTacToeGrid[2, 0] == "O" && ticTacToeGrid[2, 1] == "O" && ticTacToeGrid[2, 2] == "O")
+                || (ticTacToeGrid[0, 0] == "X" && ticTacToeGrid[1, 0] == "X" && ticTacToeGrid[2, 0] == "X")
+                || (ticTacToeGrid[0, 1] == "X" && ticTacToeGrid[1, 1] == "X" && ticTacToeGrid[2, 1] == "X")
+                || (ticTacToeGrid[0, 2] == "X" && ticTacToeGrid[1, 2] == "X" && ticTacToeGrid[2, 2] == "X")
+                || (ticTacToeGrid[0, 0] == "O" && ticTacToeGrid[1, 0] == "O" && ticTacToeGrid[2, 0] == "O")
+                || (ticTacToeGrid[0, 1] == "O" && ticTacToeGrid[1, 1] == "O" && ticTacToeGrid[2, 1] == "O")
+                || (ticTacToeGrid[0, 2] == "O" && ticTacToeGrid[1, 2] == "O" && ticTacToeGrid[2, 2] == "O")
+                || (ticTacToeGrid[0, 0] == "X" && ticTacToeGrid[1, 1] == "X" && ticTacToeGrid[2, 2] == "X")
+                || (ticTacToeGrid[0, 2] == "X" && ticTacToeGrid[1, 1] == "X" && ticTacToeGrid[2, 0] == "X")
+                || (ticTacToeGrid[0, 0] == "O" && ticTacToeGrid[1, 1] == "O" && ticTacToeGrid[2, 2] == "O")
+                || (ticTacToeGrid[0, 2] == "O" && ticTacToeGrid[1, 1] == "O" && ticTacToeGrid[2, 0] == "O"))
                 return true;
             else
                 return false;
         }
 
-        public bool FreeSpot(int row, int column)
+        public bool CheckFreeSpot(int row, int column)
         {
             if (ticTacToeGrid[row, column] != null)
-                return false;
-            else
                 return true;
+            else
+                return false;
+        }
+
+
+        public UIElement GetGridElement(Grid g, int r, int c)
+        {
+            for (int i = 0; i < g.Children.Count; i++)
+            {
+                UIElement e = g.Children[i];
+                if (Grid.GetRow(e) == r && Grid.GetColumn(e) == c)
+                    return e;
+            }
+            return null;
         }
     }
 }
